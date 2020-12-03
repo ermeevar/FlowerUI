ff983092246e0397ed1b39306cb2ed95ba6c41cf
unknown <ermeevar@1305-15.mck-ktits.ru>
2020-12-03 16:11:59 +0300

    Added Montserrat-fonts. Fixed listview on storeMAinMenu

--- lib/screens/store.main_menu.dart
+++ lib/screens/store.main_menu.dart
@@ -3,8 +3,9 @@ import 'package:flower_ui/models/store.dart';
 import 'package:flutter/material.dart';
 
 class StoreMainMenu extends StatelessWidget {
-  Store _store = new Store("Твой букетик", "Свежие цветочки", "89008004050");
-  List<Shop> _shops =[
+  Store _store =
+      new Store("Букет столицы", "Свежие цветочки", "8(900)800-40-50");
+  List<Shop> _shops = [
     new Shop("г. Казань, Мавлекаева 67", 0),
     new Shop("г. Казань, Проспект Победы 30", 0),
     new Shop("г. Казань, Солнечный город 1, 2 этаж", 0)
@@ -15,66 +16,82 @@ class StoreMainMenu extends StatelessWidget {
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
         home: Container(
-          color: Colors.white,
-          alignment: Alignment.center,
-          child: Column(
-              children: [
+            color: Colors.white,
+            alignment: Alignment.center,
+            child: Column(children: <Widget>[
               new Container(
-                color: Colors.blue,
+                color: Color.fromRGBO(130, 147, 153, 100),
+                padding: const EdgeInsets.all(20),
                 child: new Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    //new Image()?,
+                    new CircleAvatar(
+                      radius: 60,
+                      backgroundImage: NetworkImage(
+                          'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+                      backgroundColor: Colors.transparent,
+                    ),
                     new Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
                       children: [
-                        new Text(
-                          _store.name,
-                          style: new TextStyle(
-                            fontSize: 20.0,
-                            fontFamily: "Arial",
-                            color: Colors.white
-                          )
-                        ),
-                        new Text(
-                          _store.firstPhone,
-                          style: new TextStyle(
-                            fontSize: 15.0,
-                            fontFamily: "Arial",
-                            color: Colors.white
-                          )
-                        ),
-                        new Text(
-                          _store.secondPhone == null? " " : _store.secondPhone,
-                          style: new TextStyle(
-                            fontSize: 15.0,
-                            fontFamily: "Arial",
-                            color: Colors.white
-                          )
-                        ),
-                        new GestureDetector(
-                          child: new Text(
-                            "Изменить",
+                        new Text(_store.name,
                             style: new TextStyle(
-                              fontSize: 15.0,
-                              fontFamily: "Arial",
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold
-                            )
-                          )
-                        )
+                                fontSize: 20.0,
+                                fontFamily: "Montserrat",
+                                color: Colors.white,
+                                fontWeight: FontWeight.normal,
+                                decoration: TextDecoration.none)),
+                        new Text(_store.firstPhone,
+                            style: new TextStyle(
+                                height: 2,
+                                fontSize: 15.0,
+                                fontWeight: FontWeight.normal,
+                                fontFamily: "Montserrat",
+                                color: Colors.white,
+                                decoration: TextDecoration.none)),
+                        new Text(
+                            // _store.secondPhone == null
+                            //     ? " "
+                            //     : _store.secondPhone,
+                            "8(950)245-78-78",
+                            style: new TextStyle(
+                                fontSize: 15.0,
+                                fontWeight: FontWeight.normal,
+                                fontFamily: "Montserrat",
+                                color: Colors.white,
+                                decoration: TextDecoration.none)),
+                        new GestureDetector(
+                            child: new Text("Изменить",
+                                style: new TextStyle(
+                                    height: 2,
+                                    fontSize: 15.0,
+                                    fontFamily: "MontserratBold",
+                                    color: Colors.white,
+                                    decoration: TextDecoration.none)))
                       ],
                     )
                   ],
                 ),
               ),
-
-
-          ]
-        )
-        )
-    );
+              new Expanded(
+                  child: ListView.builder(
+                itemCount: _shops.length,
+                itemBuilder: (context, index) {
+                  return new Container(
+                      height: 50,
+                      child: new Text(
+                        _shops[index].address,
+                        style: new TextStyle(
+                            fontFamily: "Arial",
+                            fontSize: 15,
+                            color: Colors.black,
+                            decoration: TextDecoration.none),
+                      ));
+                },
+              ))
+            ])));
   }
 }
