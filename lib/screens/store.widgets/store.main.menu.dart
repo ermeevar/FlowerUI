eecaa1a3b5265e09859483f3a1e6d394188c5f38
ermeeva.r@gmail.com <ermeeva.r@gmail.com>
2021-04-09 03:26:13 +0300

    Added checking the ethernet connection function

--- lib/screens/store.widgets/store.main.menu.dart
+++ lib/screens/store.widgets/store.main.menu.dart
@@ -1,3 +1,4 @@
+import 'package:connectivity/connectivity.dart';
 import 'package:flower_ui/models/account.dart';
 import 'package:flower_ui/models/store.dart';
 import 'package:flower_ui/screens/store.widgets/store.content.dart';
@@ -18,6 +19,43 @@ class StoreMainMenu extends StatelessWidget {
 
     account = Account();
     account.id = 1;
+  }
+
+  checkConnection() async {
+    var connectivityResult = await (Connectivity().checkConnectivity());
+    if (connectivityResult != ConnectivityResult.mobile &&
+        connectivityResult != ConnectivityResult.wifi) {
+      await _showConnectionError();
+    }
+  }
+
+  Future<void> _showConnectionError() async {
+    return showDialog<void>(
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: SingleChildScrollView(
+            child: Text('Нет соединения с интернетом', style: Theme.of(context).textTheme.body1,),
+          ),
+          actions: <Widget>[
+            Container(
+              padding: EdgeInsets.all(10),
+              child: FlatButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: new Text(
+                  "Закрыть",
+                  style: Theme.of(context).textTheme.body1.copyWith(
+                    color: Color.fromRGBO(130, 147, 153, 1),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
